a70958accf41977abde5d3a9d759097afdfddff1
Bright Atsighi <brytebee@gmail.com>
2024-08-05 05:21:09 +0100

    Basic styline and cleanups

--- app/about.tsx
+++ app/about.tsx
@@ -1,11 +1,5 @@
 import BlogCard from "@/components/BlogList";
-import { Text } from "react-native";
 
 export default function About({}) {
-  return (
-    <>
-      <Text>About page</Text>
-      <BlogCard />
-    </>
-  );
+  return <BlogCard />;
 }
--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 interface Props {
   props: {
@@ -10,10 +11,36 @@ interface Props {
 
 export default function BlogCard({ props: { name, age, role } }: Props) {
   return (
-    <View>
-      <Text>{name}</Text>
-      <Text>{age}</Text>
-      <Text>{role}</Text>
+    <View style={styles.container}>
+      <View style={styles.userInfo}>
+        <Text style={styles.text}>{name}</Text>
+        <Text style={styles.text}>{age}</Text>
+        <Text style={styles.text}>{role}</Text>
+      </View>
+      <Feather style={styles.icon} name="trash" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: "gray",
+    borderTopWidth: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 50,
+  },
+  userInfo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flex: 1,
+  },
+  text: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
